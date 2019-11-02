84a2c342544c2c2e4648dd38c21b4bb610527a04
Git-horse <47929284+Git-horse@users.noreply.github.com>
2019-11-02 21:19:55 +0300

    +Pr5(invisible)

---
+++ Pr5/js/scripts.js
@@ -0,0 +1,27 @@
+jQuery("document").ready(function($){
+    $("#target").blur(function(){
+        if (parseInt($('#target').val()) >= 1 && parseInt($('#target').val()) <= 100){
+            $("#target").css("background-color", "green");
+        } else {
+            $("#target").css("background-color", "red");
+        }
+    });
+
+    var nabor = "0123456789qwertyuiopasdfghjklzxcvbnm";
+    var len = nabor.length;
+    $("#shmiak").on("click", function(){
+        var rez = "";
+        for (var i = 0; i < 8; i++){
+            rez += nabor[Math.floor(Math.random()*(len))];
+        }
+        $('#dich').val(rez);
+    });
+
+    window.setInterval(function(){
+        if ($("#text").css("color") == "rgb(255, 0, 0)"){
+            $("#text").css("color", "green");
+        } else{
+            $("#text").css("color", "red");
+        }
+      }, 1000);
+});
