b66242272dca286664cf271188e4da235eefcdf4
Git-horse <47929284+Git-horse@users.noreply.github.com>
2020-02-25 23:46:36 +0200

    Pr 7 (not deploy)

---
+++ Pr7/1.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("div div").css("color", "red");
+    $("#meadow").css({"color": "green", "font-size": "20px"});
+    $(".rainbow").css({"color": "yellow", "font-size": "40px"});
+    $("#fut #future").css({"color": "blue"});
+    $("[set]").css({"color": "grey"});  
+    $("[last^='code']").css({"color": "purple"});
+});
---
+++ Pr7/2.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#pmtog").css("color", "green");
+    $("p.pr1").css("color", "red");
+    $("input[type^='button']").css("background-color", "orange");
+    $(".pmb1, .pmb2, .pmb3").css("color", "blue");
+    $("#wrap1 p.pmtobr").css("color", "brown");
+    $("img[src^='https://static.turbosquid.com/Preview/2019/02/13__02_28_56/F0000.jpgD95D3345-02D5-41EA-92C6-D5A9EB2E249EZoom.jpg']").css("border-style", "solid");
+    $("div#iwrap input[type^='button'], input[type^='submit']").css("background-color", "red");
+});
